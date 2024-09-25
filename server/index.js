ca7adef3aabd46daa928facbc736f89f20e40545
zxcursed <volovykartem@gmail.com>
2024-09-25 22:42:35 +0300

    created controllers

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
  const cors = require('cors');
  const cookieParser = require('cookie-parser');
  const mongoose = require('mongoose');
+ const router = require('./router/index');
 
  const PORT = process.env.PORT || 5000;
  const app = express();
@@ -10,6 +11,7 @@
  app.use(express.json());
  app.use(express.json());
  app.use(cors());
+ app.use('/api', router);
 
  const start = async () => {
     try {
