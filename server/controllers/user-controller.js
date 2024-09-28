5900ddb8d4da08b392d4ffafd9cbc9f0c030b670
zxcursed <volovykartem@gmail.com>
2024-09-28 15:35:08 +0300

    react structure

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -31,7 +31,10 @@ class UserController {
 
     async logout(req, res, next){
         try {
-
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token);
         } catch (e) {
             next(e);
         }
@@ -49,7 +52,9 @@ class UserController {
 
     async refresh(req, res, next){
         try {
-
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken)
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
         } catch (e) {
             next(e);
         }
@@ -57,7 +62,8 @@ class UserController {
 
     async getUsers(req, res, next){
         try {
-            res.json(['123', '123', '123']);
+            const users = await userService.getAllUsers();
+            return res.json(users);
         } catch (e) {
             next(e);
         }
