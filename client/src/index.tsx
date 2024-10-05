fabe7d0209426dee1a779f978191b3c30f6f5fd0
VoroN5k <Volovykartem@gmail.com>
2024-10-06 01:06:15 +0300

    typescript ctructure stores and interfaces

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Store from "./store/store";
+import { createContext } from 'react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+interface State{
+    store: Store,
+}
+
+const store = new Store();
+
+export const Context = createContext<State>({
+    store,
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+
 
